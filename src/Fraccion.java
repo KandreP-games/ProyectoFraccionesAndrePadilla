e17317a4397e4e9ba52ec0104f701bb0f8a2f042
kleber24021 <kleber24021@gmail.com>
2021-05-24 16:12:24 +0200

    Commit funciones suma, multiplicacion y division

--- src/Fraccion.java
+++ src/Fraccion.java
@@ -9,19 +9,34 @@ public class Fraccion {
 	}
 /*
  * 	Añadir las siguientes operaciones a Fracción
- * 
- * 
- * 
+ * No implementamos la función simplifica porque ya se ha hecho en simplificarFracciones
+ * Aun así, la utilizamos en las funciones de suma, multiplicación y división
+ * CUIDADO al hacer merge en la branch develop
+ */
 	public static Fraccion simplifica(Fraccion entrada) {
+		return entrada;
 	}
-	
+
 	public static Fraccion suma(Fraccion sumando1, Fraccion sumando2) {
+		int denominadorComun = sumando1.getDenominador() * sumando2.getDenominador();
+
+		int numerador1 = sumando1.getNumerador() * sumando2.getDenominador();
+		int numerador2 = sumando2.getNumerador() * sumando1.getDenominador();
+
+		Fraccion suma = new Fraccion(numerador1 + numerador2, denominadorComun);
+
+		return simplifica(suma);
 	}
+
 	public static Fraccion multiplicacion(Fraccion sumando1, Fraccion sumando2) {
+		return simplifica(new Fraccion(sumando1.getNumerador() * sumando2.getDenominador(), sumando1.getDenominador() * sumando2.getNumerador()));
 	}
+
 	public static Fraccion division(Fraccion sumando1, Fraccion sumando2) {
+
+		return simplifica(new Fraccion(sumando1.getNumerador() * sumando2.getNumerador(), sumando1.getDenominador() * sumando2.getDenominador()));
 	}
-*/
+
 	public int getNumerador() {
 		return numerador;
 	}
@@ -41,9 +56,7 @@ public class Fraccion {
 
 	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(numerador).append(" / ").append(denominador);
-		return sb.toString();
+		return numerador + " / " + denominador;
 	}
 
 }
